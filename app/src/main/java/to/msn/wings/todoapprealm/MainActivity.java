9866fab4e9e66cbe41bf39e12a8edd107370359f
kadotaoru <daiyaogata898@gmail.com>
2020-09-29 21:49:38 +0900

    Initial commit

---
+++ app/src/main/java/to/msn/wings/todoapprealm/MainActivity.java
@@ -0,0 +1,44 @@
+package to.msn.wings.todoapprealm;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //RealmDBを取得、変数realmに格納
+        Realm.init(this);
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder().build();
+        Realm realm = Realm.getInstance(realmConfig);
+    }
+
+
+}
